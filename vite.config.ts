b32f62c4dee0c3b407fc9e190d59fef097ff82e8
HamadAltamimi8 <hamadaltamimi8@gmail.com>
2026-01-06 21:42:50 +0300

    Update to version 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Taqdeem/',
+  // تأكد أن هذا الاسم يطابق اسم المستودع (Repository) الخاص بك تماماً
+  base: './', 
   define: {
-    // هذا السطر يمنع خطأ "process is not defined" الذي يسبب الشاشة البيضاء
     'process.env': {}
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true,
   }
 })
